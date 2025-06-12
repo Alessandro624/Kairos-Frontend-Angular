f3bd59d86015c5dc1ef4737c2960c22ef00cfeb4
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-06-12 11:54:59 +0200

    Created Angular project

---
+++ KairosFrontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  // TODO add home, login, sign-in, forgot-password, admin/organizer/profile components, 404 e 403 for forbidden e not found
+];
