d591bb073e34af38a7c6ac41a06b5071fef25276
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-06-14 10:29:55 +0200

    Added profile, 401 and 403 pages

--- KairosFrontend/src/app/app.routes.ts
+++ KairosFrontend/src/app/app.routes.ts
@@ -4,13 +4,19 @@ import {AuthCallback} from './auth-callback/auth-callback';
 import {Home} from './home/home';
 import {ForgotPassword} from './forgot-password/forgot-password';
 import {Register} from './register/register';
+import {Profile} from './profile/profile';
+import {NotFound} from './not-found/not-found';
+import {Unauthorized} from './unauthorized/unauthorized';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: Home},
   {path: 'login', component: Login},
   {path: 'auth/callback', component: AuthCallback},
   {path: 'forgot-password', component: ForgotPassword},
   {path: 'register', component: Register},
-  // TODO add admin/organizer/profile components, 404 e 403 for forbidden e not found
+  {path: 'profile', component: Profile},
+  {path: '403', component: Unauthorized},
+  {path: '401', component: NotFound},
+  {path: '**', redirectTo: '/401'}
 ];
